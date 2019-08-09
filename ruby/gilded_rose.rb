007dfe690f36092203d70a940648dca4c9f77d26
Rory Collins <rory.collins@codurance.com>
2019-08-09 08:43:41 +0100

    Refactor behaviour

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -1,4 +1,5 @@
 require File.join(File.dirname(__FILE__), 'item')
+require File.join(File.dirname(__FILE__), 'gilded_rose_behaviour')
 
 class Item
 
@@ -33,58 +34,6 @@ class Item
 
   def is_backstage_pass?
     self.name == "Backstage passes to a TAFKAL80ETC concert"
-  end
-end
-
-class UpdateBehaviour
-  def update_quality item
-    item.quality-=1
-    if item.sell_in < 0
-      item.quality  -= 1
-    end
-    if item.quality < 0
-      item.quality = 0
-    end
-  end
-  def update_sell_in item
-    item.sell_in -= 1
-  end
-end
-
-class SulfurasBehaviour < UpdateBehaviour
-  def update_quality item
-  end
-  def update_sell_in item
-  end
-end
-
-class AgedBrieBehaviour < UpdateBehaviour
-  def update_quality item
-    if item.sell_in < 0
-      item.quality += 1
-    end
-    item.quality += 1
-    if item.quality > 50
-      item.quality = 50
-    end
-  end
-end
-
-class BackstagePassBehaviour < UpdateBehaviour
-  def update_quality item
-      item.quality += 1
-      if item.sell_in < 10
-        item.quality += 1
-      end
-      if item.sell_in < 5
-        item.quality += 1
-      end
-      if item.sell_in < 0
-        item.quality = 0
-      end
-      if item.quality > 50
-        item.quality = 50
-      end
   end
 end
 
